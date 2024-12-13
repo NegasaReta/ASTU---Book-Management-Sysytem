45bda8134c0aa3bf304c40180c50efec9de61999
Mahfuz Redwan <mredwan.kemal@gmail.com>
2024-12-13 20:47:13 +0300

    added functionality to the books filter using javascript

---
+++ JS/app.js
@@ -0,0 +1,23 @@
+const books= document.querySelectorAll(".book-container");
+const filter= document.querySelector(".filter-items");
+
+filter.addEventListener("click",(event)=>{
+    if (!event.target.classList.contains("active") && event.target.classList.contains("filter-item")) {
+       document.querySelector(".active").classList.remove("active");
+       event.target.classList.add("active");
+      
+    }
+    const filterValue= event.target.getAttribute("data");
+    books.forEach(book=>{
+      if (event.target.classList.contains("filter-item")) {
+        if (book.classList.contains(filterValue) || filterValue==="All")  {
+            book.classList.remove("hide");
+            book.classList.add("show");
+        }else{
+            book.classList.remove("show");
+            book.classList.add("hide");
+        }
+      }
+    })
+
+})
